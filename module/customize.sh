6728893f82af7e0538fa2fbac1cdd205b56a7b11
heinu <heinu333@gmail.com>
2023-12-16 09:10:15 +0800

    修复clash.config

--- module/customize.sh
+++ module/customize.sh
@@ -6,7 +6,7 @@ system_gid="1000"
 system_uid="1000"
 clash_data_dir="/data/clash"
 modules_dir="/data/adb/modules"
-config="false" #更新是否替换clash.config
+config="true" #更新是否替换clash.config
 ABI=$(getprop ro.product.cpu.abi)
 mkdir -p ${clash_data_dir}/run
 mkdir -p ${clash_data_dir}/clashkernel
