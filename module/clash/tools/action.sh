eefd00627381fdd79aa30457acc26d4de346ded6
Elysia <118418714+elysias123@users.noreply.github.com>
2025-01-12 19:05:35 +0800

    fix: 修复action.sh

--- module/clash/tools/action.sh
+++ module/clash/tools/action.sh
@@ -1,11 +1,10 @@
-status=$(ps -A | grep clash)
+#!/system/bin/sh
 
-if [ "${status}" ]; then
+pid=$(curl -sL http://127.0.0.1:9090 | grep hello)
+if [[ "${pid}" ]]; then
     echo "正在停止akashaProxy."
     /data/clash/scripts/clash.service -k && /data/clash/scripts/clash.iptables -k
 else
     echo "正在启动akashaProxy."
     /data/clash/scripts/clash.service -s && /data/clash/scripts/clash.iptables -s
 fi
-
-
